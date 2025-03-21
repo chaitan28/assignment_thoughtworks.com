53ea054652d8757dd2f77afdcf373b97eb4efed3
chaitan28 <n.chaitanya2803@gmail.com>
2025-03-21 21:20:42 +0530

    interview

--- infra/base/blob.tf
+++ infra/base/blob.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_storage_account" "public-storage-account" {
   name                     = "${var.prefix}psa"
   resource_group_name      = data.azurerm_resource_group.azure-resource.name
@@ -9,7 +8,7 @@ resource "azurerm_storage_account" "public-storage-account" {
 
 resource "azurerm_storage_container" "public-storage-container" {
   name                  = "${var.prefix}psc"
-  storage_account_name  = azurerm_storage_account.public-storage-account.name
+  storage_account_name  = azurerm_storage_account.public-storage-account.name  
   container_access_type = "blob"
 }
 
@@ -21,7 +20,9 @@ resource "azurerm_storage_blob" "blob-static" {
 }
 
 output "url_blob" {
-  value = "https://${azurerm_storage_account.public-storage-account.name}.blob.core.windows.net/${azurerm_storage_container.public-storage-container.name}/static/"
+  value = "https://${azurerm_storage_account.public-storage-account.name}.blob.core.windows.net/${azurerm_storage_container.public-storage-container.name}/static"
 }
+
+
 
 
