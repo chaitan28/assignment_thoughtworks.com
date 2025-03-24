98ce3362e09cb76d6f6d9c50ea65ce5d3c6baa74
chaitan28 <n.chaitanya2803@gmail.com>
2025-03-24 20:31:23 +0530

    interview

--- infra/news/modules/vm/variables.tf
+++ infra/news/modules/vm/variables.tf
@@ -4,7 +4,7 @@ variable "location" {}
 variable "vm_size" { default = "Standard_B1s" }
 variable "network_interface_id" {}
 variable "identity_id" {}
-variable "os_sku" { default = "20.04 LTS - x64 Gen2" }
+variable "os_sku" { default = "20.04 LTS" }
 variable "os_version" { default = "latest" }
 variable "container_image" {}
 
