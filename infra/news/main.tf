b4ad29279bacd543af8ad7461980425163918efd
chaitan28 <n.chaitanya2803@gmail.com>
2025-03-24 19:47:31 +0530

    interview

--- infra/news/main.tf
+++ infra/news/main.tf
@@ -1,3 +1,37 @@
+data "azurerm_user_assigned_identity" "identity-acr" {
+  resource_group_name = data.azurerm_resource_group.azure-resource.name
+  name                = "identity-acr"
+}
+
+data "azurerm_storage_account" "public-storage-account" {
+  name                = "${var.prefix}psa"
+  resource_group_name = data.azurerm_resource_group.azure-resource.name
+}
+
+data "azurerm_storage_container" "public-storage-container" {
+  name                 = "${var.prefix}psc"
+  storage_account_name = data.azurerm_storage_account.public-storage-account.name
+}
+
+data "azurerm_network_interface" "network-interface-quotes" {
+  name                = "network-interface-quotes"
+  resource_group_name = data.azurerm_resource_group.azure-resource.name
+}
+
+data "azurerm_network_interface" "network-interface-newsfeed" {
+  name                = "network-interface-newsfeed"
+  resource_group_name = data.azurerm_resource_group.azure-resource.name
+}
+
+data "azurerm_network_interface" "network-interface-frontend" {
+  name                = "network-interface-frontend"
+  resource_group_name = data.azurerm_resource_group.azure-resource.name
+}
+
+locals {
+  url_static_blob = "https://${data.azurerm_storage_account.public-storage-account.name}.blob.core.windows.net/${data.azurerm_storage_container.public-storage-container.name}"
+}
+
 module "quotes_vm" {
   source               = "./modules/vm"
   vm_name              = "quotes"
@@ -26,4 +60,8 @@ module "frontend_vm" {
   network_interface_id = data.azurerm_network_interface.network-interface-frontend.id
   identity_id          = data.azurerm_user_assigned_identity.identity-acr.id
   container_image      = "${var.prefix}frontend${var.acr_url_default}/${var.prefix}frontend:latest"
+}
+
+output "frontend_url" {
+  value = "http://${azurerm_linux_virtual_machine.virtual-machine-frontend.public_ip_address}:8080"
 }
