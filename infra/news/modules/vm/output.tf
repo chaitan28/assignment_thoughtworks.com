2b850763b94e77e1ca94a53ac59f70bbda77bf64
chaitan28 <n.chaitanya2803@gmail.com>
2025-03-24 20:09:04 +0530

    interview

--- infra/news/modules/vm/output.tf
+++ infra/news/modules/vm/output.tf
@@ -1,3 +1,9 @@
 output "frontend_url" {
-  value = "http://${azurerm_linux_virtual_machine.virtual-machine-frontend.public_ip_address}:8080"
+  value = "http://${azurerm_linux_virtual_machine.vm.public_ip_address}:8080"
+}
+output "frontend_url" {
+  value = "http://${azurerm_linux_virtual_machine.vm.public_ip_address}:8081"
+}
+output "frontend_url" {
+  value = "http://${azurerm_linux_virtual_machine.vm.public_ip_address}:8082"
 }
