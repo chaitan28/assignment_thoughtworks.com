b41d216f84e4d222f4a415fdbf59695056eeff0f
chaitan28 <n.chaitanya2803@gmail.com>
2025-03-24 20:13:11 +0530

    interview

--- infra/news/modules/vm/output.tf
+++ infra/news/modules/vm/output.tf
@@ -1,9 +1,9 @@
 output "frontend_url" {
   value = "http://${azurerm_linux_virtual_machine.vm.public_ip_address}:8080"
 }
-output "frontend_url" {
+output "newsfeed_url" {
   value = "http://${azurerm_linux_virtual_machine.vm.public_ip_address}:8081"
 }
-output "frontend_url" {
+output "quotes_url" {
   value = "http://${azurerm_linux_virtual_machine.vm.public_ip_address}:8082"
 }
